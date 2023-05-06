a1d221be4f466d9767c48eaecfe39f446612e510
Viktoria <aliceinapple99@gmail.com>
2023-05-06 18:37:38 +0300

    feat: add Welcome page component; add styles to Welcome page

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import styles from '../../styles/Main.module.scss'
 // import dynamic from 'next/dynamic'
 // const MediaQuery = dynamic(() => import('react-responsive'), {
 //   ssr: false,import NavBar from './../NavBar/index';
@@ -12,14 +13,14 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <>
+    <div className={styles.app}>
       {/* <MediaQuery minWidth={1025}>
         <Header />
       </MediaQuery> */}
       <Header />
       {children}
       <Footer />
-    </>
+    </div>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,64 @@
-import { Inter } from 'next/font/google'
-import styles from './Main.module.scss'
+import styles from '../styles/Main.module.scss'
+import Link from 'next/link'
 import Meta from '@/components/Meta'
 import Layout from '@/components/Layout'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { coldarkDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
-const inter = Inter({ subsets: ['latin'] })
+const query = `query Library {
+  books {
+    title
+    author
+    genre
+    publishedYear
+  }
+}`
+
+const response = `{
+  "data": {
+    "books": [
+      {
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee",
+        "genre": "Fiction",
+        "publishedYear": 1960
+      },
+      {
+        "title": "1984",
+        "author": "George Orwell",
+        "genre": "Fiction",
+        "publishedYear": 1949
+      }
+    ]
+  }
+}
+`
+
+const welcomeText = `This app makes it easy to interact with the API and get the data you need. With GraphQL, you can precisely specify what data you want, making your queries more efficient and reducing the amount of data sent over the wire. Try our app now and start building amazing things with GraphQL!`
 
 export default function WelcomePage() {
   return (
     <>
       <Meta title="Welcome" description="GraphQL editor" />
       <Layout>
-        <main className={`${styles.main} ${inter.className}`}>
-          <div className={styles.description}>
-            <h1>Welcome page</h1>
+        <main className={`${styles.main} ${styles.welcomePage}`}>
+          <div className={styles.welcomeInfo}>
+            <h1>Welcome to our GraphQL-powered app!</h1>
+            <span>{welcomeText}</span>
+            <button className={styles.navBtn}>
+              <Link href="/auth" className={styles.navLink}>
+                Get started
+              </Link>
+            </button>
+          </div>
+          <div className={styles.codeExample}>
+            <span className="codeTitle">Code example:</span>
+            <SyntaxHighlighter language="graphql" style={coldarkDark}>
+              {query}
+            </SyntaxHighlighter>
+            <SyntaxHighlighter language="graphql" style={coldarkDark}>
+              {response}
+            </SyntaxHighlighter>
           </div>
         </main>
       </Layout>
